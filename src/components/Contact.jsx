7d38767b79690fc2f236bb90f0d4a4919741c0aa
dhruvin <dhruvin.200410116086@gmail.com>
2023-05-11 23:46:50 +0530

    project component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,48 +2,47 @@ import React, { useState } from 'react'
 
 import './Contact.css'
 function Contact() {
-    const [user,setUser]=useState({
-        name:"",
-        email:"",
-        contact:"",
-        message:""
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        contact: "",
+        message: ""
     });
 
     function click() {
-        document.querySelector("#sucess").textContent = "Submitted sucessfully!!!"
     }
-    let name,value
-    const getUserData=(event)=>{
+    let name, value
+    const getUserData = (event) => {
         // console.log('changing');
-        name=event.target.name;
-        value=event.target.value;
-        setUser({...user,[name]:value});
+        name = event.target.name;
+        value = event.target.value;
+        setUser({ ...user, [name]: value });
     }
-    const postData= async(e)=>{
-        const{ name,email,contact,message}=user
+    const postData = async (e) => {
+        const { name, email, contact, message } = user
         e.preventDefault();
-         let response=await  fetch('https://portfolio-contact-e11ce-default-rtdb.firebaseio.com/contactDB.json',{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
+        let response = await fetch('https://portfolio-contact-e11ce-default-rtdb.firebaseio.com/contactDB.json', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
             },
-            body:JSON.stringify({
+            body: JSON.stringify({
                 name,
                 email,
                 contact,
                 message
-
+                
             })
-         });
-         if (response) {
+        });
+        if (response) {
             setUser({
-                name:"",
-                email:"",
-                contact:"",
-                message:""
+                name: "",
+                email: "",
+                contact: "",
+                message: ""
             })
-            alert("data send sucessfully");
-         }
+            document.querySelector("#sucess").textContent = "Submitted Sucessfully!!!"
+        }
     }
     return (
         <>
@@ -51,22 +50,22 @@ function Contact() {
 
                 <h2>Wanna have chat? Let's connect</h2>
 
+                    <span id='sucess'>   </span>
 
-        <form action="POST" method='POST'>
+                <form action="POST" method='POST'>
 
-                <div className="form">
-                    <input className="inputs" type="text" placeholder="enter your Name" value={user.name} onChange={getUserData} name="name"/>
-                    <input className="inputs" type="text" placeholder="enter your email" value={user.email} onChange={getUserData} name="email" required/>
-                    <input className="inputs" type="text" placeholder="enter your contact no." value={user.contact} onChange={getUserData} name="contact"/>
-                    <textarea className="inputs" placeholder="drop your message" id="message" cols="50" rows="10" value={user.message} onChange={getUserData} name="message" ></textarea>
-                </div>
-                <div className="checkbox">
-                    <input type="checkbox" id="vehicle1" />
-                    <label htmlFor='vehicle1'>don't worry! We will not share your data to anyone </label><br />
-                </div>
-                <button id="btnsubmit" onClick={postData}>Submit</button>
-                <span id='sucess'></span>
-        </form>
+                    <div className="form">
+                        <input className="inputs" type="text" placeholder="enter your Name" value={user.name} onChange={getUserData} name="name" />
+                        <input className="inputs" type="text" placeholder="enter your email" value={user.email} onChange={getUserData} name="email" required />
+                        <input className="inputs" type="text" placeholder="enter your contact no." value={user.contact} onChange={getUserData} name="contact" />
+                        <textarea className="inputs" placeholder="drop your message" id="message" cols="50" rows="10" value={user.message} onChange={getUserData} name="message" ></textarea>
+                    </div>
+                    <div className="checkbox">
+                        <input type="checkbox" id="vehicle1" />
+                        <label htmlFor='vehicle1'>don't worry! We will not share your data to anyone </label><br />
+                    </div>
+                    <button id="btnsubmit" onClick={postData}>Submit</button>
+                </form>
             </div>
         </>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,146 +1,128 @@
 import React from 'react'
 
 import './Projects.css'
+import projectimg from '../../assets/projectbg.png'
 function Projects() {
     return (
         <div>
-        <div className="project-component" id='projects'>
-            <h1>Projects</h1>
+            <div className="project-component" id='projects'>
+                <h1>Projects</h1>
 
-        <div className="project-container">
+                <div className="project-container">
                     <div className="project-card" id="project1">
-                        <div className="project-number project-number-right">01</div>
-                        <div className="project-content project-content-left">
-
-                            <div className="project-skills-container">
-                               
+                       
+                        <div className="project-content ">
+                            <div className="project-img">
+                                <img src="../../assets/projectbg.png" alt="projectbg" height={23} width={23} />
                             </div>
-
                             <h2 className="project-heading">MyWheather</h2>
 
-                            <p className="project-subHeading">App which tells you wheather of any city across the globe
+                            <p className="project-about">App which tells you wheather of any city across the globe
                             </p>
-                            <div className="btn-grp">
+                            <div className="project-skills-container">
+                                HTML,CSS,JS
+                            </div>
+                            <div className="btn-group">
                                 <a href="https://djv03wheatherapp.netlify.app/" target="_blank">
 
-                                    <button className="btn-pink btn-project">View Live</button>
+                                    <button className=" btn-project">View Live</button>
                                 </a>
                                 <a href="">
-                                    <i title="GitHubLink" className="fa-brands fa-github icon"></i>
+                                    <i title="GitHubLink" className="fa-brands fa-github icon btn-project">view code</i>
                                 </a>
-                                <a href="">
-                                    <i title="Live Link" className="fa-solid fa-link icon"></i>
-                                </a>
+                               
                             </div>
                         </div>
                     </div>
 
                     <div className="project-card" id="project2">
-                        <div className="project-number project-number-left">02</div>
+                       
+                        <div className="project-img">
+                            <img src="../../assets/projectbg.png" alt="projectbg" height={23} width={23} />
+                        </div>
                         <div className="project-content project-content-right">
-                            <div className="project-skill-container">
-                            
-                               
-                            </div>
+                           
                             <h2 className="project-heading">Rock-Paper-Scissor</h2>
-                            <div className="project-sub-heading">
-                                CLASSIC GAME, NEW WAY
-                                <p>
+                            <div className="project-about">
+                                <p className='project-about'>
                                     Your past time game made with HTML,CSS AND Javascript
                                 </p>
+                            </div>
+                            <div className="project-skill-container">
+                                HTML,css ,JS
                             </div>
                             <div className="btn-group">
                                 <a href="https://djv03rockpaperscissor.netlify.app/" target="_blank">
                                     <button className="btn-pink btn-project">View Live</button>
                                 </a>
                                 <a href="">
-                                    <i title="GitHubLink" className="fa-brands fa-github icon"></i>
+                                    <i title="GitHubLink" className="fa-brands fa-github icon btn-project">view code</i>
                                 </a>
-                                <a href="">
-                                    <i title="Live Link" className="fa-solid fa-link icon"></i>
-                                </a>
+                               
                             </div>
                         </div>
                     </div>
 
                     <div className="project-card" id="project3">
-                        <div className="project-number project-number-right">03</div>
-                        <div className="project-content project-content-left">
-                            <div className="project-skill-container">
-                            
-                              
-                            </div>
+                        
+                        <div className="project-img">
+                            <img src="../../assets/projectbg.png" alt="projectbg" height={23} width={23} />
+                        </div>
+                        <div className="project-content project-content-right">
+                          
 
                             <h2 className="project-heading">React Calculator</h2>
-                            <p className="project-sub-heading">
+                            <p className="project-about">
                                 Calculator made with react Hooks and state
                             </p>
+                            <div className="project-skill-container">
+                                HTML,css ,JS
+                            </div>
                             <div className="btn-group">
                                 <a href="https://djv03reactcalculator.netlify.app/" target="_blank">
 
                                     <button className="btn-pink btn-project" >View Live</button>
                                 </a>
+                                
                                 <a href="">
-                                    <i title="GitHubLink" className="fa-brands fa-github icon"></i>
+                                    <i title="GitHubLink" className="fa-brands fa-github icon btn-project">view code</i>
                                 </a>
-                                <a href="">
-                                    <i title="Live Link" className="fa-solid fa-link icon"></i>
-                                </a>
+                               
                             </div>
                         </div>
                     </div>
 
                     <div className="project-card" id="project4">
-                        <div className="project-number project-number-left">04</div>
+                        
+                        <div className="project-img">
+                            <img src="../../assets/projectbg.png" alt="projectbg" height={23} width={23} />
+                        </div>
                         <div className="project-content project-content-right">
-                            <div className="project-skill-container">
                            
-                              
-                            </div>
                             <h2 className="project-heading">project</h2>
-                            <p className="project-sub-heading">
+                            <p className="project-about">
                                 this is the project regarfing the greatest million $ react app of all time
                             </p>
+                            <div className="project-skill-container">
+                                HTML,css ,JS
+                            </div>
                             <div className="btn-group">
                                 <button className="btn-pink btn-project">View Live</button>
                                 <a href="">
-                                    <i title="GitHubLink" className="fa-brands fa-github icon"></i>
+                                    <i title="GitHubLink" className="fa-brands fa-github icon btn-project">view code</i>
                                 </a>
-                                <a href="">
-                                    <i title="Live Link" className="fa-solid fa-link icon"></i>
-                                </a>
+                                
                             </div>
                         </div>
                     </div>
 
-                    <div className="project-card" id="project5">
-                        <div className="project-number project-number-left">04</div>
-                        <div className="project-content project-content-right">
-                            <div className="project-skill-container">
-                           
-                              
-                            </div>
-                            <h2 className="project-heading">project</h2>
-                            <p className="project-sub-heading">
-                                this is the project regarfing the greatest million $ react app of all time
-                            </p>
-                            <div className="btn-group">
-                                <button className="btn-pink btn-project">View Live</button>
-                                <a href="">
-                                    <i title="GitHubLink" className="fa-brands fa-github icon"></i>
-                                </a>
-                                <a href="">
-                                    <i title="Live Link" className="fa-solid fa-link icon"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    
+
+
                 </div>
 
-            
 
-        </div>
+
+            </div>
 
         </div>
     )
